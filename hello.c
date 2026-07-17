346c363edb0cbd95bed77621e05924f1a02d4cca
yogisneh-stack <yogisneh14@gmail.com>
2026-07-18 01:45:50 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 10, sum;
+
+    // Calculate sum
+    sum = a + b;
+
+    // Print output
+    printf("Hello World!\n");
+    printf("Sum of %d and %d is: %d\n", a, b, sum);
+
+    return 0;
+}
